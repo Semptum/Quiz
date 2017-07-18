3903f15d2e895190eee4f7dfed7489295f8816a6
Semptum <andre.kalouguine.ak@gmail.com>
2017-07-19 01:21:45 +0200

    Bootstrap-enabled toolbar

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^dashboard/', views.index, name = "dashboard"),
+    url(r'^available/', views.index, name = "available"),
+    url(r'^settings', views.index, name = "settings"),
+    url(r'^login', views.index, name = "login"),
     url(r'^$', views.index, name = "index"),
-    url(r'^([0-9]{2})', views.nb),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(Request):
-    ENTETE="Salut"
-    fin="aurevoir"
-    return HttpResponse(ENTETE+fin)
+def index(request):
+    return render(request, 'quiz/dashboard.html', {})
 
-def nb(request,n):
-    context={'Title':n}
-    return render(request,'quiz/index.html',context)
