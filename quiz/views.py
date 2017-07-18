5e38cad3d88704ea51444652234457f0e47d7f79
AntheaL <jltmarrie@gmail.com>
2017-07-18 18:05:13 +0200

    Added dispatchers to app

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(Request):
+    ENTETE="Salut"
+    fin="aurevoir"
+    return HttpResponse(ENTETE+fin)
+
+def nb(Request, n):
+    return HttpResponse(n)
