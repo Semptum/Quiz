959b954794fff395be454a74f50880f6d9462eeb
AntheaL <jltmarrie@gmail.com>
2017-07-20 14:08:04 +0200

    Added dispatchers to app (correction)

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^dashboard/', views.index, name = "dashboard"),
-    url(r'^available/', views.index, name = "available"),
-    url(r'^settings', views.index, name = "settings"),
-    url(r'^login', views.index, name = "login"),
+    url(r'^dashboard/', views.dashboard, name = "dashboard"),
+    url(r'^available/', views.available, name = "available"),
+    url(r'^settings/', views.settings, name = "settings"),
+    url(r'^login/', views.login, name = "login"),
     url(r'^$', views.index, name = "index"),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -3,5 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    return render(request, 'quiz/index.html', {})
+
+def available(request):
     return render(request, 'quiz/dashboard.html', {})
 
+def dashboard(request):
+    return render(request, 'quiz/dashboard.html', {})
+
+def settings(request):
+    return render(request, 'quiz/dashboard.html', {})
+
+def login(request.POST):
+    return render(request, 'quiz/dashboard.html', {})
